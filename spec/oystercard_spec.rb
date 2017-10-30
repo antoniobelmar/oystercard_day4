3d9d2f8f91f574b2dea4e6283d29a555002c471e
antonio <antoniobelmardacosta@gmail.com>
2017-10-30 14:41:35 +0000

    Adds oystercard class and ensures that balance is set to 0 once it is instantiated

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
+require "oystercard"
+
 describe Oystercard do
 
+  describe "#balance" do
+    it "responds to balance method" do
+      expect(subject).to respond_to(:balance)
+    end
+
+    it "a new instance of an oystercard has a balance of 0" do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
