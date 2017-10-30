dfa67e15c55151d4711ab1895a063ecc2fec70b1
Allan Wazacz <cazwazacz@gmail.com>
2017-10-30 14:58:33 +0000

    Creates top_up method for topping up balance of card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,4 +11,14 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
+
+  describe "#top_up" do
+    it 'responds to top_up' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+    it 'when topping up a new oystercard with 10, balance increases to 10' do
+      subject.top_up(10)
+      expect(subject.balance).to eq(10)
+    end
+  end
 end
